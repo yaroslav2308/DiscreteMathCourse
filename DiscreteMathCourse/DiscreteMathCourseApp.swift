738bad7199bd21b25f45c47cc4d066bc05db598d
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2022-05-09 16:30:44 +0300

    Initial Commit

---
+++ DiscreteMathCourse/DiscreteMathCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscreteMathCourseApp.swift
+//  DiscreteMathCourse
+//
+//  Created by Yaroslav Monastyrev on 09.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiscreteMathCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
